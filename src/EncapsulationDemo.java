7141e52bcaabb50e54881c34378a01160e6d4d1f
Manish Kumar Yadav <59701564+manish-kumar5848@users.noreply.github.com>
2021-09-16 13:14:08 +0530

    new

---
+++ src/EncapsulationDemo.java
@@ -0,0 +1,52 @@
+import com.sun.source.tree.BreakTree;
+
+public class EncapsulationDemo {
+    public static void main(String [] args){
+        Player player= new Player("Rahul",100,"Sword");
+        int damage=10;
+       player.looseHealth(damage);
+      System.out.println( player.getName() + " Health is "+ player.healthRemaining());
+}
+}
+
+
+class  Player {
+    private String name;
+    private int  health;
+    private String weapon;
+
+    public Player(String name, int health, String weapon) {
+        this.name = name;
+        this.health = health;
+        this.weapon = weapon;
+    }
+
+    public void looseHealth(int damage){
+        this.health= this.health -damage;
+        if(this.health <=0){
+            System.out.println("Player is knocked out");
+        }
+    }
+    public int healthRemaining(){
+        return this.health;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public void setWeapon(String weapon) {
+        this.weapon = weapon;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getWeapon() {
+        return weapon;
+    }
+}
